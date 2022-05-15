e5d7fc2541179b088a78a65a71d1c204af0cf00f
Antonio Nicolás Lois Sanmartín <sanmartin.antonio01@gmail.com>
2022-05-15 14:31:40 +0200

    Organización recursos
    Implemento de los botones login,registro

--- SMASH-TOPO/app/src/main/java/com/example/smash_topo/MainActivity.java
+++ SMASH-TOPO/app/src/main/java/com/example/smash_topo/MainActivity.java
@@ -12,13 +12,26 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton boton_login,boton_registro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boton_login= findViewById(R.id.boton_login);
+        boton_registro= findViewById(R.id.boton_registro);
+
+        boton_login.setOnClickListener(view -> {
+            
+        });
+
 
     }
 
