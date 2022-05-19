f16427d96cf73bca61f939e30009c86e86506591
antoniolois <sanmartin.antonio01@gmail.com>
2022-05-19 13:56:32 +0200

    REGISTRO COMPLETADO Y FUNCIONAL

--- SMASH-TOPO/app/src/main/java/com/example/smash_topo/Registro.java
+++ SMASH-TOPO/app/src/main/java/com/example/smash_topo/Registro.java
@@ -3,6 +3,7 @@ package com.example.smash_topo;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
@@ -17,10 +18,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.regex.Pattern;
 
 public class Registro extends AppCompatActivity {
@@ -42,10 +46,7 @@ public class Registro extends AppCompatActivity {
         fecha_registro= findViewById(R.id.fecha_registro); // FECHA DE REGISTRO
         boton_registar= findViewById(R.id.boton_registar); // BOTON PARA REGISTRARSE
 
-
         fireBaseAutenticacion = FirebaseAuth.getInstance();
-
-
 
         // CON EL SIGUIENTE CÓDIGO SE OBTIENE LA FECHA ACTUAL, Y SE REGISTRA AL USUARIO CON ELLA
         Date fechaActual = new Date();
@@ -86,8 +87,33 @@ public class Registro extends AppCompatActivity {
     private void RegistrarUsuario(String correoElectronico, String contraseñaCorreo, String nombreUsuario) {
         fireBaseAutenticacion.createUserWithEmailAndPassword(correoElectronico,contraseñaCorreo)
                 .addOnCompleteListener(task -> { // EN CASO DE QUE EL REGISTRO SE EFECTUE DE MANERA CORRECTA SE MUESTRA UN MENSAJE EN PANTALLA
-                    if(task.isSuccessful()){
-                        FirebaseUser user = fireBaseAutenticacion.getCurrentUser();
+                    if(task.isSuccessful()) {
+                        FirebaseUser user = fireBaseAutenticacion.getCurrentUser(); //registro del usuario actual
+
+                        int contadorTopos = 0;
+                        assert user != null; // COMPRUEBA QUE EL USUARIO NO SEA NULO
+                        String uidStringUser = user.getUid();
+                        String correoStringUser = editTextTextEmailAddress.getText().toString();
+                        String contraseñaStringUser = editTextTextPassword.getText().toString();
+                        String nombreStringUser = editTextTextPersonName.getText().toString();
+                        String fechaStringUser = fecha_registro.getText().toString();
+
+                        HashMap<Object, Object> DatosPLAYER = new HashMap<>(); // ESTO PERMITE ASIGNAR CLAVES A LOS VALORES PARA PODER ENVIARLO A FIREBASE
+                        DatosPLAYER.put("UID", uidStringUser);
+                        DatosPLAYER.put("CORREO ELECTRONICO", correoStringUser);
+                        DatosPLAYER.put("CONTRASEÑA", contraseñaStringUser);
+                        DatosPLAYER.put("NOMBRE", nombreStringUser);
+                        DatosPLAYER.put("FECHA", fechaStringUser);
+                        DatosPLAYER.put("Topos", contadorTopos);
+
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        DatabaseReference reference = database.getReference("DATABASE JUGADORES REGISTRADOS");
+                        reference.child(uidStringUser).setValue(DatosPLAYER); //almacenamiento de los datos del jugador
+                        startActivity(new Intent(Registro.this,Menu.class));
+                        Toast.makeText(Registro.this, "USUARIO REGISTRADO CORRECTAMENTE", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }else{
+                        Toast.makeText(Registro.this, "HA OCURRIDO UN ERROR", Toast.LENGTH_SHORT).show();
                     }
 
 
