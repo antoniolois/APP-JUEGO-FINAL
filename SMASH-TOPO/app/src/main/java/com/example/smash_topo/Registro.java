d1c19d7bb349e2b117db15421fe5a653a9245b57
antoniolois <sanmartin.antonio01@gmail.com>
2022-05-19 11:54:17 +0200

    LIMPIEZA CÓDIGO, REGISTRO, ORGANIZACIÓN LAYOUTS
    - depuración, limpieza y organización del codigo
    - cambio en algunos diseños de layout
    - inicio del codigo de registro en Registro.class, validacion de datos introducidos, inicio en firebase.

--- SMASH-TOPO/app/src/main/java/com/example/smash_topo/Registro.java
+++ SMASH-TOPO/app/src/main/java/com/example/smash_topo/Registro.java
@@ -1,14 +1,100 @@
 package com.example.smash_topo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Pattern;
 
 public class Registro extends AppCompatActivity {
 
+    // DECLARACIÓN DE VARIABLES
+    EditText editTextTextEmailAddress, editTextTextPassword, editTextTextPersonName;
+    TextView fecha_registro;
+    Button boton_registar;
+
+    FirebaseAuth fireBaseAutenticacion; // AUTENTICACIÓN DE FIREBASE
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        editTextTextEmailAddress= findViewById(R.id.editTextTextEmailAddress); // CORREO ELECTRONICO
+        editTextTextPassword= findViewById(R.id.editTextTextPassword); // CONTRASEÑA
+        editTextTextPersonName= findViewById(R.id.editTextTextPersonName); // NOMBRE
+        fecha_registro= findViewById(R.id.fecha_registro); // FECHA DE REGISTRO
+        boton_registar= findViewById(R.id.boton_registar); // BOTON PARA REGISTRARSE
+
+
+        fireBaseAutenticacion = FirebaseAuth.getInstance();
+
+
+
+        // CON EL SIGUIENTE CÓDIGO SE OBTIENE LA FECHA ACTUAL, Y SE REGISTRA AL USUARIO CON ELLA
+        Date fechaActual = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yy"); // DIA - MES - AÑO
+        String fechaActualFinal = dateFormat.format(fechaActual);
+        fecha_registro.setText("Fecha actual:\n"+fechaActualFinal);
+
+
+        // CLICK EN BOTON REGISTRASE PARA REALIZAR REGISTRO
+        boton_registar.setOnClickListener(view -> {
+            // VARIABLES
+            String correoElectronico = editTextTextEmailAddress.getText().toString();
+            String contraseñaCorreo = editTextTextPassword.getText().toString();
+            String nombreUsuario = editTextTextPersonName.getText().toString();
+
+        // COMPROBACION DE LOS DATOS INTRODUCIDOS
+
+            //CORREO ELECTRONICO
+            if (!Patterns.EMAIL_ADDRESS.matcher(correoElectronico).matches()){  // ESTA LINEA COMPRUEBA QUE EL FORMATO DEL CORREO ES EL CORRECTO
+                editTextTextEmailAddress.setError("Correo NO válido");
+                editTextTextEmailAddress.setFocusable(true); //// ESTA LINEA OBLIGA A ESCRIBIR DE FORMA CORRECTA EL CORREO
+
+            // CONTRASEÑA
+            }else if(contraseñaCorreo.length()<5){
+                editTextTextPassword.setError("La contraseña debe contener 5 caracteres");
+                editTextTextPassword.setFocusable(true); //// ESTA LINEA OBLIGA A ESCRIBIR DE FORMA CORRECTA LA CONTRASEÑA
+            }else{
+                RegistrarUsuario(correoElectronico,contraseñaCorreo,nombreUsuario);
+            }
+
+        });
+
+
+
     }
+
+    // METODO QUE REGISTRARA A UN JUGADOR CON LOS DATOS INTRODUCIDOS
+    private void RegistrarUsuario(String correoElectronico, String contraseñaCorreo, String nombreUsuario) {
+        fireBaseAutenticacion.createUserWithEmailAndPassword(correoElectronico,contraseñaCorreo)
+                .addOnCompleteListener(task -> { // EN CASO DE QUE EL REGISTRO SE EFECTUE DE MANERA CORRECTA SE MUESTRA UN MENSAJE EN PANTALLA
+                    if(task.isSuccessful()){
+                        FirebaseUser user = fireBaseAutenticacion.getCurrentUser();
+                    }
+
+
+                }).addOnFailureListener(e -> { // EN CASO DE QUE EL REGISTRO SE EFECTUE DE MANERA INCORRECTA SE MUESTRA UN MENSAJE EN PANTALLA
+            Toast.makeText(Registro.this, "Error: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+    }
+
+
 }
