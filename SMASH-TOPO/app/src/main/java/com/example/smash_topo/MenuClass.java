752e520ada787e192cc44ff60cea8bf63e9e2f7f
antoniolois <sanmartin.antonio01@gmail.com>
2022-05-26 21:01:19 +0200

    Inicio base de datos, continuación pantalla menú, y adaptación

--- SMASH-TOPO/app/src/main/java/com/example/smash_topo/MenuClass.java
+++ SMASH-TOPO/app/src/main/java/com/example/smash_topo/MenuClass.java
@@ -1,22 +1,34 @@
 package com.example.smash_topo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class MenuClass extends AppCompatActivity {
 
     //DECLARACION DE VARIABLES
     FirebaseAuth auth;
     FirebaseUser user;
-    Button CerrarSesion;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseJugadoresRegistrados;
+
+    Button CerrarSesion,Jugar,Clasificaciones;
+    TextView cantidadTopos,userID,userName,correoElectronicoUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +39,34 @@ public class MenuClass extends AppCompatActivity {
         auth= FirebaseAuth.getInstance();
         user= auth.getCurrentUser();
 
-        CerrarSesion = findViewById(R.id.boton_cerrar_sesion_menu);
+        //INSTANCIA DE LA BASE DE DATOS
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        databaseJugadoresRegistrados=firebaseDatabase.getReference("DATABASE JUGADORES REGISTRADOS");
+
+
+
+
+            CerrarSesion = findViewById(R.id.boton_cerrar_sesion_menu);
+            Jugar = findViewById(R.id.botonJugarMenu);
+            Clasificaciones = findViewById(R.id.clasifiacionJugadoresPuntuacion);
+
+            cantidadTopos = findViewById(R.id.cantidadTopos);
+            userID = findViewById(R.id.userID);
+            userName = findViewById(R.id.userName);
+            correoElectronicoUser = findViewById(R.id.correoElectronicoUser);
+
+        // CODIGO PARA JUGAR PARTIDA
+        Jugar.setOnClickListener(view -> {
+
+        });
+
+        // CODIGO PARA MIRAR CLASIFICACIONES
+        Clasificaciones.setOnClickListener(view -> {
+
+        });
+
+
+        // CODIGO PARA CERRAR SESION
         CerrarSesion.setOnClickListener(view -> {
             CloseSesion();
         });
@@ -35,6 +74,28 @@ public class MenuClass extends AppCompatActivity {
 
 
     }
+
+    // MÉTODO PARA PODER REALIZAR CONSULTAS A LA DATABASE
+    private void consultaDatabase(){
+
+        //ORDEN DE LOS USUARIOS POR CORREO ELECTRÓNICO, Y COMPARACIÓN CON EL CORREO DEL USUARIO QUE INICIE SESIÓN
+        Query query = databaseJugadoresRegistrados.orderByChild("CORREO ELECTRONICO").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot:snapshot.getChildren()){ //BUCLE PARA RECORRER TODA LA BASE DE DATOS
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
 
     //ESTE MÉTODO SE EJECUTARÁ CUANDO LA APP ESTÉ ABIERTA
     @Override
